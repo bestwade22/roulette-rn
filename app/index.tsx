30bc720802f8c81e6c4aa40964df146c5fd04859
Rex Chan <rex.chan@sc.com>
2024-10-13 23:58:07 +0800

    update rn

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Platform, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const WebviewComponent = () => {
+  return Platform.OS === 'web' ? (
+    <iframe src={process.env.EXPO_PUBLIC_API_URL} height={'100%'} width={'100%'} />
+  ) : (
+    <WebView
+      source={{ uri: process.env.EXPO_PUBLIC_API_URL }}
+      style={{ flex: 1 }}
+    />
+  );
+};
+
+export default WebviewComponent;
