271b77a64c3929defe77138a652eaf5571ce47dd
Rex Chan <rex.chan@sc.com>
2024-10-25 01:45:36 +0800

    update rn fb plugin

--- app/index.tsx
+++ app/index.tsx
@@ -5,13 +5,10 @@ import { WebView } from 'react-native-webview';
 
 const WebviewComponent = () => {
 
-  InterstitialAdManager.preloadAd(process.env.EXPO_PUBLIC_FB_PLACEMENT_ID_FULL?? '')
+  InterstitialAdManager.showAd(process.env.EXPO_PUBLIC_FB_PLACEMENT_ID_FULL?? '')
   .then((didClick) => {})
   .catch((error) => {});
-
-// Will show it if already loaded, or wait for it to load and show it.
-  InterstitialAdManager.showPreloadedAd(process.env.EXPO_PUBLIC_FB_PLACEMENT_ID_FULL?? '');
-  console.log(process.env.EXPO_PUBLIC_FB_PLACEMENT_ID_FULL)
+  
   return Platform.OS === 'web' ? (
     <iframe src={process.env.EXPO_PUBLIC_API_URL} height={'100%'} width={'100%'} />
   ) : (
