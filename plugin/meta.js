19d94c59a1a971e73b5703f5d00122411a6c4c25
Rex Chan <rex.chan@sc.com>
2024-10-28 23:35:07 +0800

    update rn ad

--- plugin/meta.js
+++ plugin/meta.js
@@ -4,6 +4,17 @@ const {
 } = require('expo/config-plugins');
 
 const withFacebookSDK = (config) => {
+  // To add dependencies in build.gradle file
+  config = withAppBuildGradle(config, (config) => {
+    const dependenciesToAdd = `implementation 'com.facebook.android:facebook-android-sdk:latest.release'`;
+    config.modResults.contents = config.modResults.contents.replace(
+      'dependencies {',
+      `dependencies {\n${dependenciesToAdd}`
+    );
+
+    return config;
+  });
+
   // To add meta-data in AndroidManifest file
   config = withAndroidManifest(config, (config) => {
     const androidManifest = config.modResults;
@@ -29,6 +40,41 @@ const withFacebookSDK = (config) => {
 
     // Update the manifest with the new meta-data elements
     androidManifest.manifest.application[0].activity = appActivityData;
+
+    // Check if the meta-data elements are already present
+    const appMetaData =
+      androidManifest.manifest.application[0]['meta-data'] || [];
+    const metaDataFacebookAppId = {
+      $: {
+        'android:name': 'com.facebook.sdk.ApplicationId',
+        'android:value': '1513513462685414', //Replace this with Meta AppId secret
+      },
+    };
+    const metaDataClientToken = {
+      $: {
+        'android:name': 'com.facebook.sdk.ClientToken',
+        'android:value': 'b52c1f219b2d8982018d1dd5f9864628', //Replace this with Meta client secret
+      },
+    };
+    // Add meta-data elements if they do not already exist
+    if (
+      !appMetaData.some(
+        (meta) => meta.$['android:name'] === 'com.facebook.sdk.ApplicationId'
+      )
+    ) {
+      appMetaData.push(metaDataFacebookAppId);
+    }
+    if (
+      !appMetaData.some(
+        (meta) => meta.$['android:name'] === 'com.facebook.sdk.ClientToken'
+      )
+    ) {
+      appMetaData.push(metaDataClientToken);
+    }
+
+    // Update the manifest with the new meta-data elements
+    androidManifest.manifest.application[0]['meta-data'] = appMetaData;
+    console.log(androidManifest.manifest);
     return config;
   });
   return config;
