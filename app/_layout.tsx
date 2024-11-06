421547fbe6c25fc15349f0aea2b5b87d2ba87890
Rex Chan <rex.chan@sc.com>
2024-11-07 01:11:51 +0800

    update rn ad

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,29 +8,45 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-import { InterstitialAdManager } from 'react-native-fbads';
+import { AdSettings, InterstitialAdManager } from 'react-native-fbads';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+export default async function RootLayout() {
   const colorScheme = useColorScheme();
+  AdSettings.setLogLevel('debug');
+  AdSettings.addTestDevice(AdSettings.currentDeviceHash);
+  const requestedStatus = await AdSettings.requestTrackingPermission();
+
+  if (requestedStatus === 'authorized' || requestedStatus === 'unavailable') {
+    AdSettings.setAdvertiserIDCollectionEnabled(true);
+    // Both calls are not related to each other
+    // FB won't deliver any ads if this is set to false or not called at all.
+    AdSettings.setAdvertiserTrackingEnabled(true);
+  }
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
-      InterstitialAdManager.preloadAd(process.env.EXPO_PUBLIC_FB_PLACEMENT_ID_FULL ?? '')
-  .then((didClick) => {alert(didClick)})
-  .catch((error) => {alert(error)});
+      InterstitialAdManager.preloadAd(
+        process.env.EXPO_PUBLIC_FB_PLACEMENT_ID_FULL ?? ''
+      )
+        .then((didClick) => {
+          alert(didClick);
+        })
+        .catch((error) => {
+          alert(error);
+        });
 
-// Will show it if already loaded, or wait for it to load and show it.
-InterstitialAdManager.showPreloadedAd(process.env.EXPO_PUBLIC_FB_PLACEMENT_ID_FULL ?? '');
-
+      // Will show it if already loaded, or wait for it to load and show it.
+      InterstitialAdManager.showPreloadedAd(
+        process.env.EXPO_PUBLIC_FB_PLACEMENT_ID_FULL ?? ''
+      );
     }
   }, [loaded]);
 
